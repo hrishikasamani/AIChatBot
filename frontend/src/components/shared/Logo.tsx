e757862137bdf4a10db6474c64c24fe02cb3378b
hrishikasamani <samani.hr@northeastern.edu>
2024-12-18 19:06:12 -0500

    NavBar

--- frontend/src/components/shared/Logo.tsx
+++ frontend/src/components/shared/Logo.tsx
@@ -1,7 +1,42 @@
 import React from "react";
+import { Link } from 'react-router-dom';
+import Typography from "@mui/material/Typography";
 
 const Logo = () => {
-    return <div>Logo</div>;
+    return (
+        <div
+        style={{
+            display: 'flex',
+            marginRight: "auto",
+            alignItems: "center",
+            gap: "15px",
+        }}
+        >
+        <Link to = {"/"}
+            style={{
+                display: 'flex',
+                marginRight: "auto",
+                alignItems: "center",
+                gap: "15px",
+            }}
+        >
+            <img src = "openai.png"
+            alt="openai"
+            width={"30px"}
+            height={"30px"}
+            className="image-inverted"/>
+        <Typography 
+            sx={{
+                display: {md:"block", sm: "none", xs: "none"},
+                mr: "auto",
+                fontWeight: "800",
+                textShadow: "2px 2px 20px #000",
+            }}>
+            <span style={{ fontSize: "20px" }}>MERN</span>-GPT
+        </Typography>
+        </Link>
+        </div>
+    )
 };
 
 export default Logo;
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App.tsx';
 import { ThemeProvider, createTheme } from '@mui/material';
 import { BrowserRouter } from 'react-router-dom';
+import { AuthProvider } from './context/AuthContext.tsx';
 
 const theme = createTheme({typography:{
   fontFamily: "Roboto Serif, serif", 
@@ -13,11 +14,12 @@ const theme = createTheme({typography:{
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
+   <AuthProvider>
     <BrowserRouter>
       <ThemeProvider theme={theme}>
         <App />
       </ThemeProvider>
     </BrowserRouter>
-    
+    </AuthProvider> 
   </React.StrictMode>
 );
